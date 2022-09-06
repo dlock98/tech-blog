cdfd356d11f0041eb77482e1e2da1c423d4bc4e0
Danielle <danielle@Danielles-MacBook-Air.local>
2022-09-05 21:40:35 -0400

    editing front end

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const path = require('path');
-const routes = require('./controllers/');
-
-
-
-app.use(express.static(path.join(__dirname, 'public')));
-
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/'));
